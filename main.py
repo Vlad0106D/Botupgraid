edc59cf7305573a53691cd462f1199600d9fa347
Vlad0106D <vladmarunich22@icloud.com>
2025-07-22 01:14:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from flask import Flask
+import threading
 
 TOKEN = "8000528742:AAHn7SEq5pS3q98VMTBJtCeXwaguMUmrYwE"
 
@@ -13,10 +14,14 @@ def index():
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Привет! Я бот.')
 
+def run_flask():
+    app.run(host="0.0.0.0", port=10000)
+
 def run_bot():
     application = ApplicationBuilder().token(TOKEN).build()
     application.add_handler(CommandHandler('start', start))
     application.run_polling()
 
 if __name__ == '__main__':
+    threading.Thread(target=run_flask).start()
     run_bot()
