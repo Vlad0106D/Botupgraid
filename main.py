d8c77ebcc6e1900c371256b1e6f7d15dd92ad487
Vlad0106D <vladmarunich22@icloud.com>
2025-07-22 23:03:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
+import os
 import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "7753750626:AAECEmbPksDUXV1KXrAgwE6AO1wZxdCMxVo"  # ← твой бот токен
+TOKEN = os.getenv("TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("👋 Привет! Я работаю.")
+    await update.message.reply_text("Бот запущен и работает!")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("🛠 Доступные команды:\n/start — запуск\n/help — помощь")
+    await update.message.reply_text("Доступные команды:\n/start — запуск\n/help — помощь")
 
-async def main():
+def main():
     app = ApplicationBuilder().token(TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
-
-    print("✅ Бот запущен...")
-    await app.run_polling()
+    app.run_polling()
 
 if __name__ == "__main__":
     asyncio.run(main())
