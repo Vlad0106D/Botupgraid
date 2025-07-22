36904c41218694814b921575408f8fad351e47d4
Vlad0106D <vladmarunich22@icloud.com>
2025-07-22 22:36:09 +0200

    Delete bot_logic.py

--- bot_logic.py
+++
@@ -1,6 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-# Обработчик команды /signal
-async def signal(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("📈 Это тестовый сигнал. Всё работает!")
